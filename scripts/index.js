42a4e10abca095426d7d56a0e069987b2c6f8dcd
Donald Yeh <Donald.Yeh@gmail.com>
2019-03-06 11:52:20 -0500

    adds all completed exercises

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,41 @@
+// worked with Eric M.
 
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
-function getWindSpeed() {}
-function getSunrise() {}
+function getLocationName(weather) {
+  const city = new Map();
+  const cityValues = Object.values(weather); // array of city values
+  return cityValues[10]; //city name is in index 10
+}
 
+function getLocationCountry(weather) {
+  console.log(weather);
+  return weather.sys.country;
+}
+function getLocationLatitude(weather) {
+  return weather.coord.lat;
+}
+function getLocationLongitude(weather) {
+  return weather.coord.lon;
+}
+function getDescription(weather) {
+  return weather.weather[0].description;
+}
+function getWindSpeed(weather) {
+  return weather.wind["speed"];
+}
+function getSunrise(weather) {
+  return weather.sys["sunrise"];
+}
 
 // Please ignore the following
 try {
-    module.exports = {
-        getLocationName,
-        getLocationCountry,
-        getLocationLatitude,
-        getLocationLongitude,
-        getDescription,
-        getWindSpeed,
-        getSunrise
-    }
-} catch (e) {
-    
-}
+  module.exports = {
+    getLocationName,
+    getLocationCountry,
+    getLocationLatitude,
+    getLocationLongitude,
+    getDescription,
+    getWindSpeed,
+    getSunrise
+  };
+} catch (e) {}
