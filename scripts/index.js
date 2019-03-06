fefd8131bb887919ff6502a85cd58008a64c0a58
Margaret O'Neill <margaret@oneillfish.com>
2019-03-06 11:27:42 -0500

    location, us, lattitude

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,26 @@
 
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
+function getLocationName(data) {
+    return data.name;
+}
+function getLocationCountry(data) {
+    return data.sys.country;
+}
+function getLocationLatitude(data) {
+    return data.coord.lat;
+
+}
+function getLocationLongitude(data) {
+    return data.coord.lon;
+}
+function getDescription(data) {
+    return weather.description;
+}
 function getWindSpeed() {}
 function getSunrise() {}
 
-
+// console.log(`The location is ${getLocationName(atlWeather)}`);
+// console.log(getLocationCountry(atlWeather));
 // Please ignore the following
 try {
     module.exports = {
