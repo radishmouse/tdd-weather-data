47b0dabc519cea95ade56c332a705f29e68535dc
Seil Cho <seilcho7@hotmail.com>
2019-03-06 11:35:13 -0500

    tests completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,26 @@
 
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
-function getWindSpeed() {}
-function getSunrise() {}
+function getLocationName(data) {
+    return data.name;
+}
+function getLocationCountry(data) {
+    return data.sys.country;
+}
+function getLocationLatitude(data) {
+    return data.coord.lat;
+}
+function getLocationLongitude(data) {
+    return data.coord.lon;
+}
+function getDescription(data) {
+    return data.weather[0].description;
+}
+function getWindSpeed(data) {
+    return data.wind.speed;
+}
+function getSunrise(data) {
+    return data.sys.sunrise;
+}
 
 
 // Please ignore the following
