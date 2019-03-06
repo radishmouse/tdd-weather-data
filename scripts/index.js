7704e050029ae0f1acaa06737d0685e8fee9f711
Anthony DiRusso <ossurid@gmail.com>
2019-03-06 11:54:10 -0500

    final version pair testing

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,48 @@
 
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
-function getWindSpeed() {}
-function getSunrise() {}
+
+
+//location should be Atlanta
+function getLocationName(data) {
+    return data.name;
+}
+
+
+
+
+//country should be US
+function getLocationCountry (data) {
+    return data.sys.country;
+}
+
+
+
+
+function getLocationLatitude(data) {
+    return data.coord.lat;
+}
+
+function getLocationLongitude(data) {
+    return data.coord.lon;
+}
+function getDescription(data) {
+    // return data.weather[0];
+    return data.weather[0].description;
+}
+
+
+
+function getWindSpeed(data) {
+    return data.wind.speed;
+}
+
+
+
+function getSunrise(data) {
+    return data.sys.sunrise;
+}
+
+
 
 
 // Please ignore the following
