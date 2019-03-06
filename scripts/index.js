f5adf43787bd8c984a9e59b5b9b17bef900274ad
Eric Wittenberg <ebwittenberg@gmail.com>
2019-03-06 11:20:23 -0500

    added all functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,26 @@
 
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
-function getWindSpeed() {}
-function getSunrise() {}
+function getLocationName(object) {
+    return object.name;
+}
+function getLocationCountry(object) {
+    return object.sys.country;
+}
+function getLocationLatitude(object) {
+    return object.coord.lat;
+}
+function getLocationLongitude(object) {
+    return object.coord.lon;
+}
+function getDescription(object) {
+    return object.weather[0].description;
+}
+function getWindSpeed(object) {
+    return object.wind.speed;
+}
+function getSunrise(object) {
+    return object.sys.sunrise;
+}
 
 
 // Please ignore the following
