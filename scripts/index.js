8afe6a2bbc72b58b3e8bca9423b34e2368f1a66f
Jocelyn Bergdorff <joce.roseb@gmail.com>
2019-03-06 11:52:22 -0500

    calculated functions to work in a test

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,38 @@
 
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
-function getWindSpeed() {}
-function getSunrise() {}
+function getLocationName(location) {
+    location = location.name;
+    return location
+}
+
+function getLocationCountry(nation) {
+    nation = nation.sys.country;
+    return nation
+}
+
+function getLocationLatitude(latitude) {
+    latitude = latitude.coord['lat'];
+    return latitude
+}
+
+function getLocationLongitude(longitude) {
+    longitude = longitude.coord['lon'];
+    return longitude
+}
+
+function getDescription(descrip) {
+    descrip = descrip.weather[0].description;
+    return descrip
+}
+
+function getWindSpeed(windSpeed) {
+    windSpeed = windSpeed.wind['speed'];
+    return windSpeed
+}
+function getSunrise(sunUp) {
+    sunUp = sunUp.sys.sunrise;
+    return sunUp
+}
 
 
 // Please ignore the following
