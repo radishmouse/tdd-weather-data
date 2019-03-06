e0fc06a9c8d196e281b85a5f8a520d6da8eedb51
Ryan Yim <yknyim@gmail.com>
2019-03-06 12:24:32 -0500

    adds functions for weather data

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,26 @@
 
 // Implement the following functions to fulfill the tests!
-function getLocationName() {}
-function getLocationCountry() {}
-function getLocationLatitude() {}
-function getLocationLongitude() {}
-function getDescription() {}
-function getWindSpeed() {}
-function getSunrise() {}
+function getLocationName(city) {
+    return city.name;
+};
+function getLocationCountry(global) {
+    return global.sys.country;
+};
+function getLocationLatitude(latit) {
+    return latit.coord.lat;
+};
+function getLocationLongitude(long) {
+    return long.coord.lon;
+};
+function getDescription(sky) {
+    return sky.weather[0].description
+};
+function getWindSpeed(air) {
+    return air.wind.speed
+};
+function getSunrise(rise) {
+    return rise.sys.sunrise
+};
 
 
 // Please ignore the following
