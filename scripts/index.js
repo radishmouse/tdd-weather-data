fe307771e9c61952c737248966f03173bc0b1622
Matt Raines <mraines4@gmail.com>
2019-03-07 13:32:12 -0500

    adds sunset and sunrise

--- scripts/index.js
+++ scripts/index.js
@@ -75,3 +75,36 @@ weatherDiv.append(temperature);
 const windSpeed = document.createElement('p');
 windSpeed.textContent = `Windspeed: ${getWindSpeed(atlWeather)}`;
 weatherDiv.append(windSpeed);
+
+
+function getSunrise (obj) {
+    return obj.sys.sunrise;
+}
+function getSunset (obj) {
+    return obj.sys.sunset;
+}
+
+function convertTime(unix_timestamp) {
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    let date = new Date(unix_timestamp*1000);
+    // Hours part from the timestamp
+    let hours = date.getHours();
+    // Minutes part from the timestamp
+    let minutes = "0" + date.getMinutes();
+    // Seconds part from the timestamp
+    let seconds = "0" + date.getSeconds();
+
+    // Will display time in 10:30:23 format
+    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+    return formattedTime
+}
+
+
+const sunrise = document.createElement('p');
+sunrise.textContent = `Sunrise: ${convertTime(getSunrise(atlWeather))}`;
+weatherDiv.append(sunrise);
+
+const sunset = document.createElement('p');
+sunset.textContent = `Sunset: ${convertTime(getSunset(atlWeather))}`;
+weatherDiv.append(sunset);
